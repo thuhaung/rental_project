e5063c360b814b5d6d018486b34c09b9ab1256e7
heroinism <thu.ha2897@gmail.com>
2022-04-27 15:51:15 +0700

    Setup client and server folders.

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import rentalRoutes from "./routes/rentals.js";
+
+const app = express();
+
+app.use("/rentals", rentalRoutes)
+
+
+app.use(bodyParser.json({limit: "50mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://mongo:mongo@cluster0.llxlu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+                .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+                .catch((error) => console.log(error.message));
