922be79e70760d0e6da8e67f75bdb1605deaa5f0
baryenmunich123 <thanhphongth132@gmail.com>
2022-05-04 10:43:40 +0700

    Basic Rental Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Homepage from "./pages/Homepage/Homepage";
+import Rentals from "./pages/Rentals/Rentals";
 
 
 const App = () => {
     return (
         <div>
           <Homepage />
+          {/* <Rentals/> */}
         </div>
     );
 }
--- client/src/pages/Rentals/Rentals.js
+++ client/src/pages/Rentals/Rentals.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import background from '../../assets/rentalBackground.png'
+import './Rentals.css'
 
 function Rentals() {
   return (
-    <div>
-        <h1>Rentals</h1>
+    <div className='container'>
+        <div className='main-container'>
+          <div className='text-container'>
+            <h1 align='center'>Find your best home</h1>
+          </div>
+        </div>
+        <div className='sub-container'>
+          <h2>Most recent listings</h2>
+        </div>
     </div>
   )
 }
