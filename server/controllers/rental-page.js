7cf97337b4b1ffe12573765f9c6dcd6bdced45aa
quang2652001 <quang2652001@gmail.com>
2022-05-07 18:07:24 +0700

    search api

--- server/controllers/rental-page.js
+++ server/controllers/rental-page.js
@@ -3,16 +3,36 @@ import Rental from '../models/rental.js';
 import {} from "dotenv/config";
 
 export const search = async(req, res) => {
-    // let user = req.query.user;
-    // rent = Number(rent);
-    console.log(typeof req.query.user)
-    Rental.find({"address.district": "4","address.city" : "Ho Chi Minh"}, (err, rentals) => {
-        if(err) {
-            res.status(500).send(err)
-        }
-        else {
-            res.json(rentals)
-        }
-    })
+    const {city, district, price, street} = req.query;
+    let rentals;
+
+    if(city) {
+        rentals = await Rental.find({"address.city" : city});
+    }
+    else {
+        rentals = await Rental.find({"address.city" : "Ho Chi Minh"});
+    }
+
+    console.log(rentals)
+
+    if(district) {
+        rentals = rentals.filter((rental) => {
+            return rental.address.district == district;
+        })
+    }
+
+    if(price) {
+        rentals = rentals.filter((rental) => {
+            return rental.rent <= price
+        })
+    }
+
+    if(street) {
+        rentals = rentals.filter((rental) => {
+            return rental.address.street == street
+        })
+    }
+
+    res.json(rentals);
     
 }
