aac279b7ec6521272d4f7625a7564e7ede8f4cb9
quang2652001 <quang2652001@gmail.com>
2022-05-22 17:49:21 +0700

    add authorization for create rental

--- server/routes/advertisement-page.js
+++ server/routes/advertisement-page.js
@@ -1,9 +1,10 @@
 import express from "express";
 import * as advertisement from "../controllers/advertisement-page.js";
+import {authenticateRentalToken} from '../middleware/authorization.js'
 
 
 const advertisementPageRouter = express.Router();
 
-advertisementPageRouter.post("/post", advertisement.newRental);
+advertisementPageRouter.post("/post", authenticateRentalToken, advertisement.newRental);
 
 export default advertisementPageRouter;
