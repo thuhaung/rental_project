4df49819ace645752db57123e613b45b4145567c
quang2652001 <quang2652001@gmail.com>
2022-05-29 00:40:32 +0700

    implement chatroom backend

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,12 +2,14 @@ import homepageRouter from './homepage.js';
 import rentalPageRouter from './rental-page.js';
 import userInfoRouter from "./user-info.js";
 import advertisementPageRouter from './advertisement-page.js';
+import chatroomRouter from './chatroom.js';
 
 
 function Router(app) {
     app.use('/rental', rentalPageRouter);
     app.use("/user", userInfoRouter);
     app.use("/advertisement", advertisementPageRouter);
+    app.use('/chatroom', chatroomRouter);
     app.use('/', homepageRouter);
 }
 
